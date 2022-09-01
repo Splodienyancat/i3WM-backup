64be4c22d933f8e2e486c9c562a7c7a4c94556f7
Splodienyancat <Splodienyancat@gmail.com>
2022-09-01 16:34:06 -0700

    UPDATESCRIPT UPDATE

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@ cp -r alacritty $HOME/.config/alacritty
 cp -r i3 $HOME/.config/i3
 cp -r picom $HOME/.config/picom
 cp -r polybar $HOME/.config/polybar
+cp -r Backgrounds $HOME/Pictures/Backgrounds
 
 echo "The script did run try doing super+r or logging into i3"
