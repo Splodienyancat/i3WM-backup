bb4d8bb71a0eb296874e65989cbe852dff520458
Splodienyancat <Splodienyancat@gmail.com>
2022-08-31 21:17:21 -0700

    UPDATESCRIPT UPDATE

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ cp -r i3 $HOME/.config/i3
 cp -r picom $HOME/.config/picom
 cp -r polybar $HOME/.config/polybar
 
-echo "Id recommend you restart your pc or do super+r but otherwise your good to go"
+echo "The script did run try doing super+r or logging into i3"
